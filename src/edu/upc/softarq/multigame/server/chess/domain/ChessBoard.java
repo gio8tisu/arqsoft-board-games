c63242127caa4a3a24dcd068469ea3d550154cee
gio8tisu <gio8.tisu@gmail.com>
2020-05-04 11:08:04 +0200

    ADD ChessBoard and ChessCoordinate body

--- src/edu/upc/softarq/multigame/server/chess/domain/ChessBoard.java
+++ src/edu/upc/softarq/multigame/server/chess/domain/ChessBoard.java
@@ -1,6 +1,21 @@
 package edu.upc.softarq.multigame.server.chess.domain;
 
 import edu.upc.softarq.multigame.server.domain.Board;
+import edu.upc.softarq.multigame.server.domain.Cell;
 
 public class ChessBoard implements Board {
+    private final Cell[][] cells;
+
+    public ChessBoard() {
+        cells = new Cell[8][8];
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                cells[i][j] = new ChessCell();
+            }
+        }
+    }
+
+    public Cell getCell(final ChessCoordinate coordinate) {
+        return cells[coordinate.getX()][coordinate.getY()];
+    }
 }
--- src/edu/upc/softarq/multigame/server/chess/domain/ChessCoordinate.java
+++ src/edu/upc/softarq/multigame/server/chess/domain/ChessCoordinate.java
@@ -3,4 +3,18 @@ package edu.upc.softarq.multigame.server.chess.domain;
 import edu.upc.softarq.multigame.server.domain.Coordinate;
 
 public class ChessCoordinate implements Coordinate {
+    private final int x, y;
+
+    public ChessCoordinate(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return this.x;
+    }
+
+    public int getY() {
+        return this.y;
+    }
 }
